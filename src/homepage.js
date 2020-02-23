b840f047922fe73af73492b8a07f46a8d24cb7ec
prod1992 <levon1.grigoryan@gmail.com>
2020-02-23 20:38:02 +0400

    css purification for homepage

--- src/homepage.js
+++ src/homepage.js
@@ -1,2 +1,27 @@
 import "./scss/homepage.scss";
 import "bootstrap-sass/assets/javascripts/bootstrap/dropdown";
+import "jquery-ui/ui/widgets/autocomplete";
+import "./js/autocomplete";
+
+(function() {
+  $(document).on("click", ".categories_dropdown [data-value] a", function(e) {
+    console.log(e);
+    e.preventDefault();
+    var cTitle = $(this).text(),
+      cId = $(this)
+        .parent()
+        .data("value"),
+      $input = $(this)
+        .parents(".form-group")
+        .find("input"),
+      $btn = $(this)
+        .parents(".form-group")
+        .find(".form-control:not(input)"),
+      $dropdown = $(this)
+        .parents(".form-group")
+        .find("[data-toggle='dropdown']");
+    $btn.text(cTitle);
+    $input.val(cId);
+    $dropdown.dropdown("toggle");
+  });
+})();
