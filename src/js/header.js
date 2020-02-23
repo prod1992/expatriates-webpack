83da684684638c2be3552222a80f8f0b3e73fa9d
prod1992 <levon1.grigoryan@gmail.com>
2020-02-23 20:52:44 +0400

    css purification for homepage

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,11 @@
 (function() {
   const $menuToggler = $(".menu_toggler");
-  $menuToggler.on("click", function(e) {
+  const $darkOverlay = $(".dark-overlay");
+  $menuToggler.on("click touchend", function(e) {
     e.preventDefault();
     $("body").toggleClass("nav-open");
+  });
+  $darkOverlay.on("click touchend", function() {
+    $("body").removeClass("nav-open");
   });
 })();
